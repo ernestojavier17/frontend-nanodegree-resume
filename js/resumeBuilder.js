cc77d70a4115db7392d35e4c80606390b512e6ae
Ernesto J Olivo <olivo.ernesto@gmail.com>
2016-01-10 17:40:18 -0500

    Resume js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name = 'Ernesto Olivo';
+var formattedName = HTMLheaderName.replace('%data%', name);
+var role = 'Senior Software Engineer';
+var formattedRole = HTMLheaderRole.replace('%data%', role);
+
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
+
