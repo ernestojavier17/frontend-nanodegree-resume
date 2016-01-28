ef1f947f867925e6f0031df0f67695ae88fda314
Ernesto Olivo <ernesto.olivo@smartmatic.com>
2016-01-27 19:50:13 -0500

    Creating resume objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,6 +6,71 @@ var formattedName = HTMLheaderName.replace('%data%', name);
 var role = 'Senior Software Engineer';
 var formattedRole = HTMLheaderRole.replace('%data%', role);
 
+var work = {
+    "jobs": [
+        {
+            employer: "",
+            "title": "Sr. Software Engineer",
+            "location": "",
+            "dates": "Panama",
+            "description": ""
+        },
+        {
+            employer: "",
+            "title": "Sr. Software Engineer",
+            "location": "",
+            "dates": "Panama",
+            "description": ""
+        }
+    ]
+}
+
+var projects = {
+    "projects": [
+        {
+            title: "",
+            dates: "",
+            description: "",
+            images: []
+        }
+    ]
+}
+
+var bio = {
+    "name": "",
+    "role": "",
+    "welcomeMessage": "",
+    "contacts": {
+        "mobile": "",
+        "email": ""
+    },
+    "skills": []
+}
+
+var education = {
+    "schools": [
+        {
+            "name": "",
+            "location": "",
+            "degree": "",
+            "major": [],
+            "dates": ""
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "",
+            "school": "",
+            "dates": "",
+            "url": ""
+        }
+    ]
+}
+
+
+
 $('#header').prepend(formattedRole);
 $('#header').prepend(formattedName);
+$('#header').append(work['position']);
+$('#header').append(education.name);
 
